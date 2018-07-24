fb4a07c40a75b016f57c3abb57f0cb336e80ccde
Elliott Marquez <emarquez@google.com>
2018-07-24 13:17:44 -0700

    cleanup element migration

--- neon-shared-element-animation-behavior.js
+++ neon-shared-element-animation-behavior.js
@@ -1,20 +1,21 @@
 /**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+This code may only be used under the BSD style license found at
+http://polymer.github.io/LICENSE.txt The complete set of authors may be found at
+http://polymer.github.io/AUTHORS.txt The complete set of contributors may be
+found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by Google as
+part of the polymer project is also subject to an additional IP rights grant
+found at http://polymer.github.io/PATENTS.txt
 */
 import '@polymer/polymer/polymer-legacy.js';
 
-import { NeonAnimationBehavior } from './neon-animation-behavior.js';
+import {NeonAnimationBehavior} from './neon-animation-behavior.js';
 
 /**
- * Use `Polymer.NeonSharedElementAnimationBehavior` to implement shared element
+ * Use `NeonSharedElementAnimationBehavior` to implement shared element
  * animations.
- * @polymerBehavior Polymer.NeonSharedElementAnimationBehavior
+ * @polymerBehavior NeonSharedElementAnimationBehavior
  */
 export const NeonSharedElementAnimationBehaviorImpl = {
 
@@ -66,8 +67,6 @@ export const NeonSharedElementAnimationBehaviorImpl = {
 
 };
 
-/** @polymerBehavior Polymer.NeonSharedElementAnimationBehavior */
-export const NeonSharedElementAnimationBehavior = [
-  NeonAnimationBehavior,
-  NeonSharedElementAnimationBehaviorImpl
-];
+/** @polymerBehavior NeonSharedElementAnimationBehavior */
+export const NeonSharedElementAnimationBehavior =
+    [NeonAnimationBehavior, NeonSharedElementAnimationBehaviorImpl];
